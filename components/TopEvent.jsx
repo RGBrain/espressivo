132569b5ae3f0e996360f3b6a010ce5bf0417e51
tarbousier <tarb@h>
2024-12-07 07:14:59 +0000

    added css background-image for TopEvent

--- components/TopEvent.jsx
+++ components/TopEvent.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 
 const TopEvent = () => {
-  return <div className="w-full bg-gray-300 h-80 hover:bg-[image:var(--public/engegard_quartet.jpeg)] focus:bg-[image:var(--public/engegard_quartet.jpeg)] ...">Most forthcoming Concert</div>;
+  return (
+    <div className="w-full bg-gray-300 h-80 div_TopEvent">
+    </div>
+  );
 }
 
 export default TopEvent;
