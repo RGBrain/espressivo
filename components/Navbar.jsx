0a7820e92fb341f7ddd69e93071e10441a438675
Richard Brain <richard.brain@hotmail.co.uk>
2024-12-18 00:14:42 +0000

    add new responsive header

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,15 +4,15 @@ import NavItem from './NavItem'
 
 export default function navbar() {
   return (
-    <nav className='esporange py-0 my-0 text-xl font-bold text-white mx-auto'>
+    <nav className='bg-orange py-0 my-0 text-xl font-bold text-white mx-auto'>
       <div className="flex">
-      <Image
-              src="/Espressivo.png"
-              width={100}
-              height={100}
+      <Image className='m-6 shrink-0'
+              src="/esp_logo_white.svg"
+              width={300}
+              height={300}
               alt="Espressivo Logo"
               />
-      <ul className="relative flex mx-2 p-6 items-center justify-between bg-esporange">
+      <ul className="relative flex mx-2 p-6 items-center justify-between bg-orange">
           <NavItem linkName="Whats On" />
           <NavItem linkName="Previous Events" />
           <NavItem linkName="About Us" />
