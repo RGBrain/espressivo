a098c83628500802516e16d2867aae25e6889af7
Richard Brain <richard.brain@hotmail.co.uk>
2025-03-14 18:34:28 +0000

    add main event card text - WIP

---
+++ components/BtnBookTickets.jsx
@@ -0,0 +1,18 @@
+import Link from 'next/link';
+import React from 'react'
+  
+const BtnBookTickets = ({concertData}) => {
+
+        const boxOfficeURL = "http://www.braintelemarketing.com/";
+
+    return (
+        <div
+            className="h-100 w-100 bg-orange absolute p-2 m-5 bottom-0 left-5 rounded-xl"
+        >
+        <Link className='text-black' href={boxOfficeURL}>Book Tickets</Link>
+        </div>
+    );
+
+  }
+
+export default BtnBookTickets;
--- components/TopEventCard.jsx
+++ components/TopEventCard.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import BtnBookTickets from './BtnBookTickets';
+
   
 const TopEventCard = ({concertData}) => {
 
@@ -13,13 +15,12 @@ const TopEventCard = ({concertData}) => {
         
     return (
         <div
-            className="w-full h-[50vh] bg-cover bg-center flex justify-center items-center"
+            className="w-full h-[50vh] bg-cover bg-center flex justify-center items-center relative"
             style={{ backgroundImage: `url(${imagePath})` }}
         >
-        <h3 className='text-2xl text-white'>{concertData.artist_entity_name}</h3>
-        <h3 className='text-2xl text-white'>{dateOfConcert.toDateString()}</h3>
-        <h3 className='text-2xl text-white'>{concertData.venue_name}</h3>
-        <h3 className='text-2xl text-white'>{concertData.venue_town}</h3>
+        <h2 className='text-5xl text-white absolute p-5 bottom-40 left-5'>{concertData.artist_entity_name}</h2>
+        <h3 className='text-2xl text-white absolute p-5 bottom-20 left-5'>{dateOfConcert.toDateString()} &nbsp;&nbsp;&nbsp; {concertData.venue_name} &nbsp;&nbsp;&nbsp; {concertData.venue_town}</h3>
+        <BtnBookTickets />
         </div>
     );
 
