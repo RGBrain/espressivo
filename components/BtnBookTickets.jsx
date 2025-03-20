fb5dc5df55c9199dc71bf34cd7f648ca71f30fe9
Richard Brain <richard.brain@hotmail.co.uk>
2025-03-20 20:00:45 +0000

    format top-event text

--- components/BtnBookTickets.jsx
+++ components/BtnBookTickets.jsx
@@ -2,11 +2,13 @@ import Link from "next/link";
 import React from "react";
 
 const BtnBookTickets = ({ BookTicketsLink }) => {
+  
+  // ! Placeholder for testing
   const boxOfficeURL = BookTicketsLink; // "http://www.braintelemarketing.com/";
 
   return (
-    <div className="h-100 w-100 bg-orange absolute p-2 m-5 bottom-0 left-5 rounded-xl">
-      <Link className="text-black" href={boxOfficeURL} target="_blank">
+    <div className="h-100 w-100 bg-orange absolute p-2 m-5 bottom-2 left-5 rounded-xl">
+      <Link className="text-black font-bold" href={boxOfficeURL} target="_blank">
         Book Tickets
       </Link>
     </div>
