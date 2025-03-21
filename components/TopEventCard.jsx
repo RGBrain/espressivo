33207a11e68c7c64f794918584e150a404b8ee00
Richard Brain <richard.brain@hotmail.co.uk>
2025-03-21 11:30:02 +0000

    reduce font weight

--- components/TopEventCard.jsx
+++ components/TopEventCard.jsx
@@ -7,8 +7,8 @@ const TopEventCard = ({ concertData }) => {
   return (
     <div className="w-full h-[50vh] bg-cover bg-center flex justify-center relative" style={{ backgroundImage: `url(${imagePath})` }}>
       <h2 className="text-5xl text-white absolute p-5 bottom-24 left-5">{concertData.artist_entity_name}</h2>
-      <h3 className="text-xl text-white absolute px-5 bottom-20 left-5">
-        {new Date(concertData.concert_timestamp * 1000).toDateString()} &nbsp;&nbsp;&nbsp; {concertData.venue_name} &nbsp;&nbsp;&nbsp; {concertData.venue_town}
+      <h3 className="text-xl text-white font-thin absolute px-5 bottom-20 left-5">
+        {new Date(concertData.concert_timestamp * 1000).toDateString()} &nbsp;&nbsp;&nbsp; {concertData.venue_name} &nbsp; {concertData.venue_town}
       </h3>
       <BtnBookTickets BookTicketsLink={concertData.concert_book_tickets_link} />
     </div>
