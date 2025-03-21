31678778af874578f109bc86914674fb21a3c183
Richard Brain <richard.brain@hotmail.co.uk>
2025-03-21 13:28:13 +0000

    complete WorksTable

--- components/TopEventInfo.jsx
+++ components/TopEventInfo.jsx
@@ -5,13 +5,14 @@ import WorksTable from "./WorksTable";
 
 // ! Placeholder data, will be dynamic, from db
 const eventOverview = "Espressivo is delighted to have engaged the superb Engegard Quartet from Norway to give two concerts in Herefordshire as part of their UK tour on Saturday 30 September and Sunday 1 October 2023."
+
+
 const TopEventInfo = ({ concertData }) => {
-    console.log(concertData.concert_works[0].composer_display_name);
   return (
     <div className="flex flex-wrap justify-center relative">
         <p className="font-bold text-orange m-5 p-5">{eventOverview}</p>
         <div className="border-b-2 border-orange w-96"></div>
-        <WorksTable concertData={concertData} />
+        <WorksTable works={concertData.concert_works} />
     </div>
   );
 };
--- components/WorksTable.jsx
+++ components/WorksTable.jsx
@@ -1,25 +1,21 @@
 import React from "react"
 
-const WorksTable = ({ concertData }) => {
+const WorksTable = ({ works }) => {
+
+  const rows = works.map(item => Object.values(item));
   
-    return (
-        <table className="table-auto m-5 p-5">
-            <tbody>
-                <tr className="m-5">
-                    <td className="p-1">{concertData.concert_works[0].composer_display_name}</td>
-                    <td className="p-1">{concertData.concert_works[0].work_title}</td>
-                </tr>
-                <tr>
-                    <td className="p-1">{concertData.concert_works[1].composer_display_name}</td>
-                    <td className="p-1">{concertData.concert_works[1].work_title}</td>
-                </tr>
-                <tr>
-                    <td className="p-1">{concertData.concert_works[2].composer_display_name}</td>
-                    <td className="p-1">{concertData.concert_works[2].work_title}</td>
-                </tr>
-            </tbody>
-        </table>
-    );
+  return (
+    <table className="table-auto m-5 p-5">
+      <tbody>
+        {rows.map((row, index) => (
+          <tr key={index}>
+            <td className="p-1">{rows[index][3]}</td>
+            <td className="p-1">{rows[index][0]}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
   };
   
   export default WorksTable;
