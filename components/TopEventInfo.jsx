81cbc2d3bef12189295bc5e11da6239bce3e4c92
tarbousier <tarb@h>
2025-03-22 21:23:45 +0000

    make orange borders by centered

--- components/TopEventInfo.jsx
+++ components/TopEventInfo.jsx
@@ -7,14 +7,14 @@ const TopEventInfo = ({ concertData }) => {
     <div className="flex flex-col sm:flex-row justify-center relative sm:gap-9">
       <div className="grow-2 basis-2/3">
         <p className="font-bold text-orange m-5 p-5">{concertData.concert_overview}</p>
-        <div className="border-b-2 border-orange border-opacity-50 w-full"></div>
+        <div className="border-b-2 border-orange border-opacity-50 mx-20"></div>
         <WorksTable works={concertData.concert_works} />
-        <div className="border-b-2 border-orange border-opacity-50 w-full"></div>
+        <div className="border-b-2 border-orange border-opacity-50 mx-20"></div>
       </div>
 
       <div className="grow-1 basis-1/3">
         <ArtistTable artist_members={concertData.artist_members} />
-        <div className="border-b-2 border-orange border-opacity-50 w-full"></div>
+        <div className="border-b-2 border-orange border-opacity-50 mx-20"></div>
         <p className="font-bold m-5 p-5">{concertData.concert_description}</p>
       </div>
     </div>
