c52dc4af07f65e9ec2742b55a9626a3ca0e11161
Richard Brain <richard.brain@hotmail.co.uk>
2025-03-22 18:39:46 +0000

    tidying

--- components/ArtistTable.jsx
+++ components/ArtistTable.jsx
@@ -1,18 +1,16 @@
 import React from "react";
 
 const ArtistTable = ({ artist_members }) => {
+
   const rows = artist_members.map((item) => Object.values(item));
 
-  // ! Need to replace the below for artist (each player) info once db is set up
-  // ? artist_members is now dynamic and setup
-
   return (
-    <table className="table-auto m-5 p-5">
+    <table className="table-fixed m-5 p-5">
       <tbody>
         {rows.map((row, index) => (
           <tr key={index}>
-            <td className="p-1">{rows[index][0]}</td>
-            <td className="p-1">{rows[index][1]}</td>
+            <td className="p-1 px-5">{rows[index][0]}</td>
+            <td className="p-1 px-5">{rows[index][1]}</td>
           </tr>
         ))}
       </tbody>
--- components/WorksTable.jsx
+++ components/WorksTable.jsx
@@ -5,12 +5,12 @@ const WorksTable = ({ works }) => {
   const rows = works.map(item => Object.values(item));
   
   return (
-    <table className="table-auto m-5 p-5">
+    <table className="table-fixed m-5 p-5">
       <tbody>
         {rows.map((row, index) => (
           <tr key={index}>
-            <td className="p-1">{rows[index][3]}</td>
-            <td className="p-1">{rows[index][0]}</td>
+            <td className="p-1 px-5">{rows[index][3]}</td>
+            <td className="p-1 px-5">{rows[index][0]}</td>
           </tr>
         ))}
       </tbody>
