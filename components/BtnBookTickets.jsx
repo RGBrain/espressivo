02f7905ebce46479413632619a37dd29854501c3
tarbousier <tarb@h>
2025-03-23 02:44:55 +0000

    make BtnBookTickets placement responsive & amend TopEvent flex code

--- components/BtnBookTickets.jsx
+++ components/BtnBookTickets.jsx
@@ -2,12 +2,11 @@ import Link from "next/link";
 import React from "react";
 
 const BtnBookTickets = ({ BookTicketsLink }) => {
-  
   // ! Placeholder for testing
   const boxOfficeURL = BookTicketsLink; // "http://www.braintelemarketing.com/";
 
   return (
-    <div className="h-100 w-100 bg-orange absolute p-2 m-5 bottom-2 left-5 rounded-xl">
+    <div className="h-100 w-100 bg-orange p-2 m-5 rounded-xl ml-9 mt-3">
       <Link className="text-black font-bold" href={boxOfficeURL} target="_blank">
         Book Tickets
       </Link>
--- components/TopEventCard.jsx
+++ components/TopEventCard.jsx
@@ -5,11 +5,14 @@ const TopEventCard = ({ concertData }) => {
   const imagePath = "/" + concertData.artist_entity_img_filename;
 
   return (
-    <div className="w-full h-[50vh] bg-cover bg-center flex justify-center relative" style={{ backgroundImage: `url(${imagePath})` }}>
-      <h2 className="text-5xl text-white absolute p-5 bottom-24 left-5">{concertData.artist_entity_name}</h2>
-      <h3 className="text-xl text-white font-thin absolute px-5 bottom-20 left-5">
-        {new Date(concertData.concert_timestamp * 1000).toDateString()} &nbsp;&nbsp;&nbsp; {concertData.venue_name} &nbsp; {concertData.venue_town}
-      </h3>
+    <div className="w-full h-[50vh] bg-cover bg-center flex flex-col lg:flex-row justify-end items-start lg:items-end lg:justify-between relative lg:pb-6 lg:pr-6 xl:pr-28" style={{ backgroundImage: `url(${imagePath})` }}>
+      <div>
+        {/* div to group artistName and date + venue */}
+        <h2 className="text-5xl text-white ml-9 mb-3">{concertData.artist_entity_name}</h2>
+        <h3 className="text-xl text-white font-thin ml-9">
+          {new Date(concertData.concert_timestamp * 1000).toDateString()} &nbsp;&nbsp;&nbsp; {concertData.venue_name} &nbsp; {concertData.venue_town}
+        </h3>
+      </div>
       <BtnBookTickets BookTicketsLink={concertData.concert_book_tickets_link} />
     </div>
   );
