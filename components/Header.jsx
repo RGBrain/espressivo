18b31a642d3adfee44ad7de0ef7ca8c2eb48197d
tarbousier <tarb@h>
2025-03-23 04:51:18 +0000

    hamburger - make it mr-0 when viewport < 400px width (to fix right white space bug)

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React from "react";
 import Image from "next/image";
 import { useState } from "react";
@@ -9,18 +9,13 @@ export default function Header() {
 
   return (
     <div className="flex items-center justify-between py-2 bg-orange p-6">
-      <Image className='m-6 shrink-0'
-              src="/esp_logo_white.svg"
-              width={250}
-              height={250}
-              alt="Espressivo Logo"
-              />
+      <Image className="m-6 shrink-1" src="/esp_logo_white.svg" width={250} height={250} alt="Espressivo Logo" />
       <nav>
         {/* Mobile menu */}
         <section className="flex lg:hidden">
           <div
-        //   Hamburger Icon
-            className="space-y-2 m-6"
+            //   Hamburger Icon
+            className="space-y-2 m-6 max-xs:mr-0"
             onClick={() => setIsNavOpen((prev) => !prev)}
           >
             <span className="block h-0.5 w-8 bg-white"></span>
@@ -29,29 +24,18 @@ export default function Header() {
           </div>
 
           <div className={isNavOpen ? "showMenuNav" : "hideMenuNav"}>
-        {/* X close icon */}
-            <div
-              className="absolute top-6 right-4 px-8 py-8"
-              onClick={() => setIsNavOpen(false)}
-            >
-              <svg
-                className="h-8 w-8 text-gray-600"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
+            {/* X close icon */}
+            <div className="absolute top-6 right-4 px-8 py-8" onClick={() => setIsNavOpen(false)}>
+              <svg className="h-8 w-8 text-gray-600" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                 <line x1="18" y1="6" x2="6" y2="18" />
                 <line x1="6" y1="6" x2="18" y2="18" />
               </svg>
             </div>
             <ul className="flex flex-col items-center justify-between min-h-[250px]">
-          <NavItem linkName="Whats On" />
-          <NavItem linkName="Previous Events" />
-          <NavItem linkName="About Us" />
-          <NavItem linkName="Contact Us" />
+              <NavItem linkName="Whats On" />
+              <NavItem linkName="Previous Events" />
+              <NavItem linkName="About Us" />
+              <NavItem linkName="Contact Us" />
             </ul>
           </div>
         </section>
