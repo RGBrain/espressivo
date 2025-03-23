6dbe985d5eff64229de173811adc8aed60c49b65
tarbousier <tarb@h>
2025-03-23 04:46:45 +0000

    tailwind.config.js - add custom breakpoint (xs:) for 400px

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./pages/**/*.{js,ts,jsx,tsx,mdx}", "./components/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       colors: {
         orange: "#cb5b09",
-        beige: "#dbd3ad", 
+        beige: "#dbd3ad",
         white: "#ffffff",
-        black: "#041003"   
+        black: "#041003",
+      },
+      screens: {
+        xs: "400px",
       },
     },
   },
   plugins: [],
 };
-
