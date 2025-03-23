9f1290811dcef3fbd9301e81555a18b2a3d4c7e0
tarbousier <tarb@h>
2025-03-23 03:05:01 +0000

    decrease text-size of ArtistTable when two columns

--- components/ArtistTable.jsx
+++ components/ArtistTable.jsx
@@ -4,12 +4,12 @@ const ArtistTable = ({ artist_members }) => {
   const rows = artist_members.map((item) => Object.values(item));
 
   return (
-    <table className="table-fixed m-5 p-5">
+    <table className="table-fixed m-5 p-5 md:text-xs lg:text-sm">
       <tbody>
         {rows.map((row, index) => (
           <tr key={index}>
-            <td className="p-1 px-5 align-top">{rows[index][0]}</td>
-            <td className="p-1 px-5 align-top">{rows[index][1]}</td>
+            <td className="p-1 px-5 align-top md:px-2">{rows[index][0]}</td>
+            <td className="p-1 px-5 align-top md:px-4">{rows[index][1]}</td>
           </tr>
         ))}
       </tbody>
